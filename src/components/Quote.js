fd895e5281de1b4f2fd7319d33200aa27beef3ec
jmayalag <jayala1993@outlook.com>
2017-02-05 15:50:58 -0300

    add decoration

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import Thumbnail from './Thumbnail'
+
+import deco from './deco.png'
+import insight from '../assets/images/insight.jpg'
 import './Quote.css'
+
+const thumb = {
+  img: insight,
+  alt: 'Insight'
+}
 
 const Quote = (props) => {
   const description = props.quote.text.split('\n').map((line, i) => <p key={i}>{line}</p>)
 
   return (
   <div className="Quote">
-    <div className="Quote-author">{props.quote.author}</div>
+    <div className="Quote-author">
+        <div className="Quote-author-title">
+          <Thumbnail image={thumb} />
+          {props.quote.author}
+        </div>
+        <img className="Quote-author-deco" src={deco} />
+    </div>
     <div className="Quote-text">
       {description}
     </div>
--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,3 +1,29 @@
-/**
- * Created by Jordan on 2/5/17.
- */
+import React, {PropTypes} from 'react'
+
+const styles = {
+  div: {
+    display: 'inline',
+    margin: '8px',
+  },
+  img: {
+    width: '60px',
+    height: '60px',
+    border: '1px groove #373938',
+    borderRadius: 2,
+  }
+}
+
+const Thumbnail = (props) => (
+  <div className="Thumbnail" style={styles.div}>
+    <img src={props.image.img} alt={props.image.alt} style={styles.img} />
+  </div>
+)
+
+Thumbnail.propTypes = {
+  image: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+  }).isRequired
+}
+
+export default Thumbnail
